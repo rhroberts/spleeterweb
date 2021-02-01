898a8f95fce704ac02aeb60d79cdbc52ec91dd93
Rusty Roberts <rust.roberts@protonmail.com>
2021-01-31 23:58:00 -0800

    initial commit

---
+++ spleeterweb/app.py
@@ -0,0 +1,38 @@
+import os
+
+from flask import Flask, send_file, request
+from werkzeug.utils import secure_filename
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_object("spleeterweb.config")
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    STATIC_DIR = app.config["STATIC_DIR"]
+
+    # a simple page that says hello
+    @app.route("/", methods=["GET", "POST"])
+    def application_root():
+        if request.method == "POST":
+            if "input_file" in request.files:
+                input_file = request.files["input_file"]
+                print(secure_filename(input_file.filename))
+            else:
+                print("no input file selected")
+
+        return send_file(os.path.join(STATIC_DIR, "index.html"), mimetype="text/html")
+
+    return app
---
+++ spleeterweb/config.py
@@ -0,0 +1,2 @@
+STATIC_DIR = "./static"
+ALLOWED_EXTENSIONS = ["wav", "flac", "mp3"]
