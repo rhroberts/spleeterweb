a384dbb17cd1ae415c69f2c68b1c70c85c201ab7
Rusty Roberts <rust.roberts@protonmail.com>
2021-02-06 12:07:06 -0800

    add env and debug options to run script; move app factory call outside main where uwsgi can find it

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
-from spleeterweb.app import app as application
+import os
+
+from spleeterweb.app import create_app
+
+FLASK_ENV = os.environ.get("FLASK_ENV", "development")
+FLASK_DEBUG = os.environ.get("FLASK_DEBUG", True)
+
+application = create_app()
 
 if __name__ == "__main__":
+    application.config["ENV"] = FLASK_ENV
+    application.config["DEBUG"] = FLASK_DEBUG
     application.run()
